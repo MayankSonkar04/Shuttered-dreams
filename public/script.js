a496d4d29cbb4a32fbc2097f33674e799571cc8c
Mayank Sonkar <mayanksonkar04@gmail.com>
2025-09-21 22:24:58 +0530

    Update script.js with new changes

--- public/script.js
+++ public/script.js
@@ -1,9 +1,18 @@
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true,
-    lerp:0.06
+    lerp:0.06,
+   
+  smooth: true,
+  smartphone: {
+    smooth: true,
+  },
+  tablet: {
+    smooth: true,
+  }
 });
 
 window.addEventListener("load", () => {
   scroll.update();  
 });
+
